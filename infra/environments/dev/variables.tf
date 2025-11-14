accdce1947e759b18d5716e6ad0f23e3af981ad9
Usermint-Network <founder@usermintnetwork.com>
2025-11-13 23:35:20 -0500

    Update variables.tf

--- infra/environments/dev/variables.tf
+++ infra/environments/dev/variables.tf
@@ -1,26 +1,18 @@
 variable "project_id" {
   type        = string
-  description = "GCP project ID"
+  description = "GCP Project ID"
 }
 
 variable "region" {
   type        = string
-  description = "GCP region"
-  default     = "us-central1"
+  description = "GCP Region"
 }
 
 variable "media_bucket_name" {
   type        = string
-  description = "Media bucket name for dev"
+  description = "Media bucket name"
 }
 
 variable "api_image" {
   type        = string
   description = "Container image for the Usermint API"
-  # Optional: pin a sensible default to the last known-good image
-  default = "us-central1-docker.pkg.dev/usermint-network/usermint/api:1763074929"
-}
-
-variable "project_id" { type = string }
-
-variable "region"     { type = string }
